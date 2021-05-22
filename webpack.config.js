c26a54776c28a4e4b518bfebf4d84d204a673704
Kanhaiya Mishra <starklm1402@gmail.com>
2021-05-22 19:20:00 +0530

    Adding server.js file to run this project on heroku URL

--- webpack.config.js
+++ webpack.config.js
@@ -21,15 +21,15 @@ module.exports = {
    },
    resolve: { extensions: ["*", ".js", ".jsx"] },
    output: {
-      path: path.resolve(__dirname, "dist/"),
-      publicPath: "/dist/",
+      path: path.resolve(__dirname, "public/dist/"),
+      publicPath: "/public/dist/",
       filename: "bundle.js"
    },
    devServer: {
       contentBase: path.join(__dirname, "/public"),
-      port: 3000,
+      port: 8080,
       historyApiFallback: true,
-      publicPath: "http://localhost:3000/dist/",
+      publicPath: "http://localhost:8080/public/dist/",
       hotOnly: true
    },
    plugins: [new webpack.HotModuleReplacementPlugin()]
